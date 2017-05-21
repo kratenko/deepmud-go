e8f690597f5342e7d3d574eb0608223335334268
kratenko <kratenko@garstig.org>
2017-05-21 14:31:46 +0200

    cobra

--- cmd/deepmud/main.go
+++ cmd/deepmud/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func Run(cmd *cobra.Command, args []string) {
+	fmt.Printf("Hello Mud!\n")
+}
 
 func main() {
-	fmt.Printf("Hello Mud!\n")
+	cmd := &cobra.Command{
+		Use:   "deepmud",
+		Short: "Going deeper into MUDs",
+		Run:   Run,
+	}
+	if err := cmd.Execute(); err != nil {
+		panic(err)
+	}
 }
